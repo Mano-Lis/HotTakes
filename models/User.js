4658edd91d0d8bf8365009a04d041372b91c460d
manolis <laptit99@hotmail.fr>
2023-02-15 16:33:31 +0100

    fix: add custom validation to email

--- models/User.js
+++ models/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        lowercase: true,
+        validate: {
+            validator: (email) => validator.isEmail(email)
+        },
+        message: props => `${props.value} is not a valid email!`
+    },
+    password: { 
+        type: String, 
+        required: true,
+        minLength: 4
+    }
 });
 
 userSchema.plugin(uniqueValidator);
